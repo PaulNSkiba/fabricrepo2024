1a49dc5bec7d9aac7711166a159c2aa9ccb1a919
PaulNSkiba <paul.n.skiba@gmail.com>
2025-05-29 14:32:40 +0200

    Committing 6 items from workspace 17d1bc90-2073-4fa5-9758-d4bb3dcd6a51

---
+++ Fabric2024/Notebook 4.Notebook/notebook-content.py
@@ -0,0 +1,34 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "4be21fb2-0ed3-4950-ade5-cf6aa7ea740e",
+# META       "default_lakehouse_name": "lh_dp700",
+# META       "default_lakehouse_workspace_id": "17d1bc90-2073-4fa5-9758-d4bb3dcd6a51",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "4be21fb2-0ed3-4950-ade5-cf6aa7ea740e"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+# Just for testing DevPipeline
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
