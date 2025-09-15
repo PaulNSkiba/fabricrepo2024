3eb78ecc7c6bc1f578c57932baf36d2553896b3b
PaulNSkiba <paul.n.skiba@gmail.com>
2025-09-15 21:29:21 +0200

    Committing 10 items from workspace 17d1bc90-2073-4fa5-9758-d4bb3dcd6a51

---
+++ Fabric2024/Notebook 5.Notebook/notebook-content.sql
@@ -0,0 +1,33 @@
+-- Fabric notebook source
+
+-- METADATA ********************
+
+-- META {
+-- META   "kernel_info": {
+-- META     "name": "sqldatawarehouse"
+-- META   },
+-- META   "dependencies": {
+-- META     "warehouse": {
+-- META       "default_warehouse": "69894195-4398-4f95-86ed-bbfe22560e8a",
+-- META       "known_warehouses": [
+-- META         {
+-- META           "id": "69894195-4398-4f95-86ed-bbfe22560e8a",
+-- META           "type": "Lakewarehouse"
+-- META         }
+-- META       ]
+-- META     }
+-- META   }
+-- META }
+
+-- CELL ********************
+
+-- Welcome to your new notebook
+-- Type here in the cell editor to add code!
+
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sql",
+-- META   "language_group": "sqldatawarehouse"
+-- META }
